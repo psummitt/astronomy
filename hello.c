8695d4bf2ee3382d8b4fba3a64bccf703f1ced96
Paul M. Summitt <psummitt@gmail.com>
2022-12-17 11:03:25 -0600

    initial

---
+++ hello.c
@@ -0,0 +1,11 @@
+/* 
+  Listing 3.1
+  helllo.c - Canonical "Hello, world!" program 4   4
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    fprintf(stdout, "Hello, Linux programming world!\n";
+    return 0;
+}
