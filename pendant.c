2b6f60ada509be06f03b4270eefd35380dd86284
Paul M. Summitt <psummitt@gmail.com>
2022-12-31 09:37:23 -0600

    initial

---
+++ pendant.c
@@ -0,0 +1,11 @@
+/*
+* Listing 3.2
+* pendant .c - use --ansi, -pendantic or -pendantic-errors
+*/
+#include <stdio.h>
+
+void main(void)
+{
+    long long int i = 01;
+    fprint(stdout, "This is a non-conforming C program\";
+}
